b581052f3f9df7c33d6f2b5c1cd2d76a5e6fe5aa
hawkilol <abacates21@gmail.com>
2023-02-05 21:46:03 -0300

    +play and stop buttomn -eslint

---
+++ src/components/FormNewTask.vue
@@ -0,0 +1,46 @@
+
+<template>
+    <div class="box">
+      <div class="columns">
+        <div
+          class="column is-8"
+          role="form"
+          aria-label="Formulário para criação de uma nova tarefa"
+        >
+          <input
+            type="text"
+            class="input"
+            placeholder="Qual tarefa você deseja iniciar?"
+            v-model="description"
+          />
+        </div>
+        <div class="column">
+          <Timer @TimerEnded="endTask"/>
+        </div>
+      </div>
+    </div>
+  </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import  Timer  from './Timer.vue'
+
+export default defineComponent({
+    name: "FormNewTask",
+    components:{
+        Timer
+    },
+    data() {
+        return {
+            description: ''
+        }
+    },
+    
+    methods: {
+        endTask (timeElapsed: number): void {
+            console.log('Task Time: ', timeElapsed)
+            console.log('Task Description');
+            this.description = ''
+        }
+    }
+});
+</script>
